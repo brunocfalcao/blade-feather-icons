98040b74d37097918052b538773295cdfaaf2acd
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-03-30 19:32:06 +0530

    added tests and new icons

---
+++ tests/CompilesIconsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Brunocfalcao\BladeFeatherIcons\BladeFeatherIconsServiceProvider;
+
+class CompilesIconsTest extends TestCase
+{
+    /** @test */
+    public function it_compiles_a_single_anonymous_component()
+    {
+        $result = svg('feathericon-wind')->toHtml();
+
+        // Note: the empty class here seems to be a Blade components bug.
+        $expected = <<<'SVG'
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-wind"><path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_classes_to_icons()
+    {
+        $result = svg('feathericon-wind', 'w-6 h-6 text-gray-500')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-wind"><path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_styles_to_icons()
+    {
+        $result = svg('feathericon-wind', ['style' => 'color: #555'])->toHtml();
+
+        $expected = <<<'SVG'
+            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-wind"><path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            BladeIconsServiceProvider::class,
+            BladeFeatherIconsServiceProvider::class,
+        ];
+    }
+}
